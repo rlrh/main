9f66cb7ba1aa461712fd1a187dc96d9a6ac03ccc
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2019-03-02 14:13:40 +0800

    Add javadoc for Network utils

--- src/main/java/seedu/address/network/Network.java
+++ src/main/java/seedu/address/network/Network.java
@@ -1,6 +1,5 @@
 package seedu.address.network;
 
-
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -10,6 +9,12 @@ import org.apache.http.client.fluent.Request;
  * Manager of Network component
  */
 public abstract class Network {
+    /**
+     * Fetches the resource (i.e. webpage) at url, returning it as an InputStream.
+     * @param url
+     * @return The input stream containing the content fetched.
+     * @throws IOException
+     */
     public static InputStream fetchAsStream(String url) throws IOException {
         return Request.Get(url)
                 .execute()
@@ -17,6 +22,12 @@ public abstract class Network {
                 .asStream();
     }
 
+    /**
+     * Fetches the resource (i.e. webpage) at url, returning it as a String.
+     * @param url
+     * @return The content fetched.
+     * @throws IOException
+     */
     public static String fetchAsString(String url) throws IOException {
         return Request.Get(url)
                 .execute()
