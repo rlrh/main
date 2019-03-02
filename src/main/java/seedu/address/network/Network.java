872240e0dfa7e346cb8ac95deba3c0f39c5e6d4c
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2019-03-02 14:08:55 +0800

    Standardise Network interface, throw exception upwards

--- src/main/java/seedu/address/network/Network.java
+++ src/main/java/seedu/address/network/Network.java
@@ -3,41 +3,24 @@ package seedu.address.network;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Optional;
-import java.util.logging.Logger;
 
 import org.apache.http.client.fluent.Request;
-
-import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.util.StringUtil;
 
 /**
  * Manager of Network component
  */
 public abstract class Network {
-    private static final Logger logger = LogsCenter.getLogger(Network.class);
-
-    public static Optional<InputStream> fetchAsStream(String url) {
-        try {
-            return Optional.of(Request.Get(url)
-                    .execute()
-                    .returnContent()
-                    .asStream());
-        } catch (IOException e) {
-            logger.warning(StringUtil.getDetails(e));
-            return Optional.empty();
-        }
+    public static InputStream fetchAsStream(String url) throws IOException {
+        return Request.Get(url)
+                .execute()
+                .returnContent()
+                .asStream();
     }
 
-    public static String fetch(String url) {
-        try {
-            return Request.Get(url)
-                    .execute()
-                    .returnContent()
-                    .asString();
-        } catch (IOException e) {
-            logger.warning(StringUtil.getDetails(e));
-            return "HTTP ERROR PAGE OR SOMETHING " + e;
-        }
+    public static String fetchAsString(String url) throws IOException {
+        return Request.Get(url)
+                .execute()
+                .returnContent()
+                .asString();
     }
 }
