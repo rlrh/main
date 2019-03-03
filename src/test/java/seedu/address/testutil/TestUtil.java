06feb3b322a89d2564eb40ac82642ab614d0b394
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2019-03-03 14:30:47 +0800

    Remove unused TestUtil#makeTempStorage

--- src/test/java/seedu/address/testutil/TestUtil.java
+++ src/test/java/seedu/address/testutil/TestUtil.java
@@ -5,15 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.junit.rules.TemporaryFolder;
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 import seedu.address.model.entry.Entry;
-import seedu.address.storage.JsonEntryBookStorage;
-import seedu.address.storage.JsonUserPrefsStorage;
-import seedu.address.storage.Storage;
-import seedu.address.storage.StorageManager;
 
 /**
  * A utility class for test cases.
@@ -57,15 +51,5 @@ public class TestUtil {
      */
     public static Entry getPerson(Model model, Index index) {
         return model.getFilteredPersonList().get(index.getZeroBased());
-    }
-
-    /**
-     * Creates a temporary Storage
-     */
-    public static Storage makeTempStorage() throws IOException {
-        TemporaryFolder temporaryFolder = new TemporaryFolder();
-        JsonEntryBookStorage addressBookStorage = new JsonEntryBookStorage(temporaryFolder.newFile().toPath());
-        JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(temporaryFolder.newFile().toPath());
-        return new StorageManager(addressBookStorage, userPrefsStorage);
     }
 }
