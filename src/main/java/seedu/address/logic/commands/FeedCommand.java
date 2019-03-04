c3d43d18ce2f49ec04ed6dff793471fe54aab510
qjqqyy <qjqqyy@gmail.com>
2019-03-05 01:29:17 +0800

    Feed: implement a command that imports the entire RSS feed

--- src/main/java/seedu/address/logic/commands/FeedCommand.java
+++ src/main/java/seedu/address/logic/commands/FeedCommand.java
@@ -3,20 +3,43 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+
+import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.entry.Address;
+import seedu.address.model.entry.Comment;
+import seedu.address.model.entry.Entry;
+import seedu.address.model.entry.Link;
+import seedu.address.model.entry.Title;
+import seedu.address.network.Network;
 
+/**
+ * Shows a feed given an URL.
+ */
 public class FeedCommand extends Command {
 
     public static final String COMMAND_WORD = "feed";
 
     public static final String MESSAGE_SUCCESS = "Opened feed %s";
-    public static final String MESSAGE_FAILURE = "Cannot open feed";
-    public static final String MESSAGE_EMPTY_URL = "URL cannot be empty";
+    public static final String MESSAGE_FAILURE_NET = "Network connection failed:\n%s";
+    public static final String MESSAGE_FAILURE_XML = "%s is not a valid RSS/Atom feed!";
+    public static final String MESSAGE_EMPTY_URL = "URL cannot be empty!";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Opens a link as an RSS feed and displays its entries" +
-            ".\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Opens a link as an RSS feed and adds all its entries.\n"
             + "Parameters: LINK\n"
             + "Example: " + COMMAND_WORD + " https://open.kattis.com/rss/new-problems";
 
@@ -29,8 +52,38 @@ public class FeedCommand extends Command {
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
 
+        try {
+            InputStream inputStream = Network.fetchAsStream(feedUrl);
+            SyndFeed syndFeed = new SyndFeedInput().build(new XmlReader(inputStream));
+            convertToEntryList(syndFeed).forEach(model::addPerson);
+            model.commitAddressBook();
+        } catch (IOException e) {
+            throw new CommandException(String.format(MESSAGE_FAILURE_NET, e), e);
+        } catch (FeedException e) {
+            throw new CommandException(String.format(MESSAGE_FAILURE_XML, feedUrl), e);
+        } catch (Exception e) {
+            throw new CommandException("Some other problem: " + StringUtil.getDetails(e), e);
+        }
+
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         return new CommandResult(String.format(MESSAGE_SUCCESS, feedUrl));
+    }
+
+    private List<Entry> convertToEntryList(SyndFeed syndFeed) {
+        return syndFeed.getEntries().stream()
+                .map(this::syndEntryToEntryBookEntry)
+                .collect(Collectors.toList());
+    }
+
+    /** Converts a SyndEntry into an EntryBook Entry. */
+    private Entry syndEntryToEntryBookEntry(SyndEntry syndEntry) {
+        return new Entry(
+                new Title(syndEntry.getTitle().trim()),
+                new Comment(syndEntry.getDescription().getValue().trim().replace('\n', ' ')),
+                new Link(syndEntry.getLink()),
+                new Address("unused"),
+                Collections.emptySet()
+        );
     }
 
     @Override
--- src/main/java/seedu/address/logic/parser/FeedCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FeedCommandParser.java
@@ -1,16 +1,19 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.logic.commands.FeedCommand.MESSAGE_EMPTY_URL;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.logic.commands.FeedCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new FeedCommand object
+ */
 public class FeedCommandParser implements Parser<FeedCommand> {
 
     @Override
     public FeedCommand parse(String userInput) throws ParseException {
         if (userInput.trim().isEmpty()) {
-            throw new ParseException(MESSAGE_EMPTY_URL);
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FeedCommand.MESSAGE_USAGE));
         }
         return new FeedCommand(userInput);
     }
