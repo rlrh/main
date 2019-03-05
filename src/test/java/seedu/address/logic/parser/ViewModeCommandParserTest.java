fb112afcd3620aff9f8f92e4260a86ad16b9d035
rlrh <rlrh1996@gmail.com>
2019-03-05 15:06:23 +0800

    Add ViewModeCommandParserTest

--- src/test/java/seedu/address/logic/parser/ViewModeCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewModeCommandParserTest.java
@@ -1,10 +1,8 @@
 package seedu.address.logic.parser;
 
 import org.junit.Test;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.model.entry.TitleContainsKeywordsPredicate;
-
-import java.util.Arrays;
+import seedu.address.logic.commands.ViewModeCommand;
+import seedu.address.ui.ViewMode;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
@@ -12,22 +10,27 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSucces
 
 public class ViewModeCommandParserTest {
 
-    private FindCommandParser parser = new FindCommandParser();
+    private ViewModeCommandParser parser = new ViewModeCommandParser();
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewModeCommand.MESSAGE_USAGE));
     }
 
     @Test
-    public void parse_validArgs_returnsFindCommand() {
+    public void parse_validArgs_returnsViewCommand() {
         // no leading and trailing whitespaces
-        FindCommand expectedFindCommand =
-                new FindCommand(new TitleContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
-        assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
+        ViewModeCommand expectedViewModeCommand =
+                new ViewModeCommand(ViewMode.BROWSER);
+        assertParseSuccess(parser, "browser", expectedViewModeCommand);
 
         // multiple whitespaces between keywords
-        assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
+        assertParseSuccess(parser, " browser   ", expectedViewModeCommand);
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "nonsense", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewModeCommand.MESSAGE_USAGE));
     }
 
 }
