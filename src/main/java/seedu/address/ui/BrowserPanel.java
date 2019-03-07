53f71de8ed0e6da85019cd2815a59d9121b54d5e
rlrh <rlrh1996@gmail.com>
2019-03-08 00:13:26 +0800

    Refactor code

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -3,7 +3,9 @@ package seedu.address.ui;
 import static java.util.Objects.requireNonNull;
 
 import java.net.URL;
+import java.util.function.Consumer;
 import java.util.logging.Logger;
+import javax.xml.transform.TransformerException;
 
 import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
@@ -15,12 +17,11 @@ import javafx.scene.web.WebView;
 import seedu.address.commons.util.XmlUtil;
 import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.entry.Entry;
 
 import com.chimbori.crux.articles.Article;
 import com.chimbori.crux.articles.ArticleExtractor;
-
-import javax.xml.transform.TransformerException;
 
 /**
  * The Browser Panel of the App.
@@ -42,70 +43,117 @@ public class BrowserPanel extends UiPart<Region> {
     private WebEngine webEngine = browser.getEngine();
 
     private String currentLocation;
-    private boolean isCurrentlyNonBrowserView;
+    private boolean isCurrentlyReaderView;
     private ViewMode viewMode;
+    private Consumer<CommandResult> onSuccess;
+    private Consumer<Exception> onFailure;
 
-    public BrowserPanel(ObservableValue<Entry> selectedPerson, ObservableValue<ViewMode> viewMode) {
+    public BrowserPanel(ObservableValue<Entry> selectedEntry,
+                        ObservableValue<ViewMode> viewMode,
+                        Consumer<CommandResult> onSuccess,
+                        Consumer<Exception> onFailure) {
+
         super(FXML);
+
+        this.currentLocation = "";
+        this.isCurrentlyReaderView = false;
+        this.viewMode = viewMode.getValue();
+        this.onSuccess = onSuccess;
+        this.onFailure = onFailure;
 
         // To prevent triggering events for typing inside the loaded Web page.
         getRoot().setOnKeyPressed(Event::consume);
 
-        this.currentLocation = "";
-        this.viewMode = viewMode.getValue();
-        this.isCurrentlyNonBrowserView = false;
-
         // Load entry page when selected entry changes.
-        selectedPerson.addListener((observable, oldValue, newValue) -> {
+        selectedEntry.addListener((observable, oldValue, newValue) -> {
             if (newValue == null) {
                 loadDefaultPage();
                 return;
             }
-            loadPersonPage(newValue);
+            loadEntryPage(newValue);
         });
 
+        // Reload when view mode changes.
         viewMode.addListener((observable, oldViewMode, newViewMode) -> {
             this.viewMode = newViewMode;
             reload();
         });
 
-        /*
-         * Load reader view if view mode is not browser view but non-browser view is not showing
-         * Load error page if error occurs.
-         */
+        // Respond to browser state events.
         webEngine.getLoadWorker().stateProperty().addListener((observable, oldState, newState) -> {
             switch (newState) {
             case RUNNING:
-                if (this.isCurrentlyNonBrowserView) {
-                    logger.info(String.format("Loading non-browser view for %s...", this.currentLocation));
-                } else {
-                    logger.info(String.format("Loading %s...", this.currentLocation));
-                }
+                handleRunning();
                 break;
             case SUCCEEDED:
-                if (this.isCurrentlyNonBrowserView) {
-                    logger.info(String.format("Successfully loaded non-browser view for %s", this.currentLocation));
-                } else {
-                    logger.info(String.format("Successfully loaded %s", this.currentLocation));
-                }
-
-                if (!this.viewMode.equals(ViewMode.BROWSER) && !this.isCurrentlyNonBrowserView) {
-                    loadReaderPage(this.currentLocation);
-                }
+                handleSucceeded();
                 break;
             case FAILED:
-                logger.warning(String.format("Failed to load %s", this.currentLocation));
-                loadErrorPage();
+                handleFailed();
                 break;
             default:
                 break;
             }
         });
 
+        // Load default page on startup.
         loadDefaultPage();
+
     }
 
-    private void loadPersonPage(Entry entry) {
+    /**
+     * Displays loading message.
+     */
+    private void handleRunning() {
+        if (isCurrentlyReaderView) {
+            String message = String.format("Loading reader view for %s...", this.currentLocation);
+            logger.info(message);
+            onSuccess.accept(new CommandResult(message));
+        } else {
+            String message = String.format("Loading %s...", this.currentLocation);
+            logger.info(message);
+            onSuccess.accept(new CommandResult(message));
+        }
+    }
+
+    /**
+     * Displays loaded message, and loads reader view if necessary.
+     */
+    private void handleSucceeded() {
+
+        // Log and display loaded message
+        if (isCurrentlyReaderView) {
+            String message = String.format("Successfully loaded reader view for %s", this.currentLocation);
+            logger.info(message);
+            onSuccess.accept(new CommandResult(message));
+        } else {
+            String message = String.format("Successfully loaded %s", this.currentLocation);
+            logger.info(message);
+            onSuccess.accept(new CommandResult(message));
+        }
+
+        // Load reader view if reader view mode is selected but not loaded
+        if (viewMode.equals(ViewMode.READER) && !isCurrentlyReaderView) {
+            loadReader(this.currentLocation);
+        }
+
+    }
+
+    /**
+     * Displays failed to load message, and loads error page.
+     */
+    private void handleFailed() {
+        String message = String.format("Failed to load %s", this.currentLocation);
+        logger.warning(message);
+        onFailure.accept(new Exception(message));
+        loadErrorPage();
+    }
+
+    /**
+     * Loads an entry.
+     * @param entry entry to load
+     */
+    private void loadEntryPage(Entry entry) {
         loadPage(entry.getLink().value);
     }
 
@@ -123,20 +171,23 @@ public class BrowserPanel extends UiPart<Region> {
         loadPage(ERROR_PAGE.toExternalForm());
     }
 
+    /**
+     * Loads a Web page.
+     * @param url URL of the Web page to load
+     */
     private void loadPage(String url) {
-        this.isCurrentlyNonBrowserView = false;
-        this.currentLocation = url;
+        isCurrentlyReaderView = false;
+        currentLocation = url;
         Platform.runLater(() -> webEngine.load(url));
     }
 
     /**
      * Loads reader view of current content.
-     * Assumes original page is already loaded.
-     * @param url base url
+     * Assumes original Web page is already loaded.
+     * @param url base URL
      */
-    private void loadReaderPage(String url) {
-        this.isCurrentlyNonBrowserView = true;
-        this.currentLocation = url;
+    private void loadReader(String url) {
+        isCurrentlyReaderView = true;
         if (url.equals(DEFAULT_PAGE.toExternalForm()) || url.equals(ERROR_PAGE.toExternalForm())) {
             return;
         }
@@ -149,21 +200,23 @@ public class BrowserPanel extends UiPart<Region> {
             String cleanHtml = article.document.outerHtml();
             Platform.runLater(() -> webEngine.loadContent(cleanHtml));
         } catch (TransformerException te) {
-            logger.warning(String.format("Failed to load reader view for %s", this.currentLocation));
+            String message = String.format("Failed to load reader view for %s", this.currentLocation);
+            logger.warning(message);
+            onFailure.accept(new Exception(message));
         }
     }
 
     /**
      * Reloads the current content.
-     * Assumes original page is already loaded.
+     * Assumes original Web page is already loaded.
      */
     private void reload() {
-        switch (this.viewMode) {
+        switch (viewMode) {
         case BROWSER:
-            loadPage(this.currentLocation);
+            loadPage(currentLocation);
             break;
         case READER:
-            loadReaderPage(this.currentLocation);
+            loadReader(currentLocation);
             break;
         default:
             break;
