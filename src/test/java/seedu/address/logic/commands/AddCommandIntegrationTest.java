91190064b85bfa14a18d88f2cfaae4e8e74673aa
rlrh <rlrh1996@gmail.com>
2019-03-17 12:57:44 +0800

    Add integration tests for AddCommand in AddCommandIntegrationTest

--- src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
@@ -2,6 +2,12 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalEntries.CRUX_LINK_FINAL;
+import static seedu.address.testutil.TypicalEntries.CRUX_LINK_NO_TITLE_NO_DESCRIPTION;
+import static seedu.address.testutil.TypicalEntries.REAL_LINK_FINAL;
+import static seedu.address.testutil.TypicalEntries.REAL_LINK_NO_TITLE_NO_DESCRIPTION;
+import static seedu.address.testutil.TypicalEntries.STUB_LINK_FINAL;
+import static seedu.address.testutil.TypicalEntries.STUB_LINK_NO_TITLE_NO_DESCRIPTION;
 
 import org.junit.Test;
 
@@ -20,7 +26,7 @@ public class AddCommandIntegrationTest {
     private CommandHistory commandHistory = new CommandHistory();
 
     @Test
-    public void execute_newPerson_success() {
+    public void execute_newEntry_success() {
         Entry validEntry = new EntryBuilder().build();
 
         Model expectedModel = model.clone();
@@ -31,7 +37,34 @@ public class AddCommandIntegrationTest {
     }
 
     @Test
-    public void execute_duplicatePerson_throwsCommandException() {
+    public void execute_stubEntryHasNoTitleAndNoDescription_titleAndDescriptionReplaced() {
+        Model expectedModel = model.clone();
+        expectedModel.addEntry(STUB_LINK_FINAL);
+
+        assertCommandSuccess(new AddCommand(STUB_LINK_NO_TITLE_NO_DESCRIPTION), model, commandHistory,
+                String.format(AddCommand.MESSAGE_SUCCESS, STUB_LINK_FINAL), expectedModel);
+    }
+
+    @Test
+    public void execute_realEntryHasNoTitleAndNoDescription_titleAndDescriptionReplaced() {
+        Model expectedModel = model.clone();
+        expectedModel.addEntry(REAL_LINK_FINAL);
+
+        assertCommandSuccess(new AddCommand(REAL_LINK_NO_TITLE_NO_DESCRIPTION), model, commandHistory,
+                String.format(AddCommand.MESSAGE_SUCCESS, REAL_LINK_FINAL), expectedModel);
+    }
+
+    @Test
+    public void execute_cruxEntryHasNoTitleAndNoDescription_titleAndDescriptionReplaced() {
+        Model expectedModel = model.clone();
+        expectedModel.addEntry(CRUX_LINK_FINAL);
+
+        assertCommandSuccess(new AddCommand(CRUX_LINK_NO_TITLE_NO_DESCRIPTION), model, commandHistory,
+                String.format(AddCommand.MESSAGE_SUCCESS, CRUX_LINK_FINAL), expectedModel);
+    }
+
+    @Test
+    public void execute_duplicateEntry_throwsCommandException() {
         Entry entryInList = model.getEntryBook().getEntryList().get(0);
         assertCommandFailure(new AddCommand(entryInList), model, commandHistory,
                 AddCommand.MESSAGE_DUPLICATE_PERSON);
