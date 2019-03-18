e34427e2c83c9dce93936935cdf39a82f47c9ec2
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2019-03-18 21:46:10 +0800

    Rename variables in JsonAdaptedEntry

--- src/main/java/seedu/address/storage/JsonAdaptedEntry.java
+++ src/main/java/seedu/address/storage/JsonAdaptedEntry.java
@@ -24,9 +24,9 @@ class JsonAdaptedEntry {
 
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "Entry's %s field is missing!";
 
-    private final String name;
-    private final String phone;
-    private final String email;
+    private final String title;
+    private final String description;
+    private final String link;
     private final String address;
     private final List<JsonAdaptedTag> tagged = new ArrayList<>();
 
@@ -34,12 +34,12 @@ class JsonAdaptedEntry {
      * Constructs a {@code JsonAdaptedEntry} with the given entry details.
      */
     @JsonCreator
-    public JsonAdaptedEntry(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
-                            @JsonProperty("email") String email, @JsonProperty("address") String address,
+    public JsonAdaptedEntry(@JsonProperty("title") String title, @JsonProperty("description") String description,
+                            @JsonProperty("link") String link, @JsonProperty("address") String address,
                             @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
+        this.title = title;
+        this.description = description;
+        this.link = link;
         this.address = address;
         if (tagged != null) {
             this.tagged.addAll(tagged);
@@ -50,9 +50,9 @@ class JsonAdaptedEntry {
      * Converts a given {@code Entry} into this class for Jackson use.
      */
     public JsonAdaptedEntry(Entry source) {
-        name = source.getTitle().fullTitle;
-        phone = source.getDescription().value;
-        email = source.getLink().value;
+        title = source.getTitle().fullTitle;
+        description = source.getDescription().value;
+        link = source.getLink().value;
         address = source.getAddress().value;
         tagged.addAll(source.getTags().stream()
                 .map(JsonAdaptedTag::new)
@@ -70,30 +70,30 @@ class JsonAdaptedEntry {
             personTags.add(tag.toModelType());
         }
 
-        if (name == null) {
+        if (title == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Title.class.getSimpleName()));
         }
-        if (!Title.isValidConstructionTitle(name)) {
-            throw new IllegalValueException(Title.formExceptionMessage(name));
+        if (!Title.isValidConstructionTitle(title)) {
+            throw new IllegalValueException(Title.formExceptionMessage(title));
         }
-        final Title modelTitle = new Title(name);
+        final Title modelTitle = new Title(title);
 
-        if (phone == null) {
+        if (description == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
                                                           Description.class.getSimpleName()));
         }
-        if (!Description.isValidConstructionDescription(phone)) {
-            throw new IllegalValueException(Description.formExceptionMessage(phone));
+        if (!Description.isValidConstructionDescription(description)) {
+            throw new IllegalValueException(Description.formExceptionMessage(description));
         }
-        final Description modelDescription = new Description(phone);
+        final Description modelDescription = new Description(description);
 
-        if (email == null) {
+        if (link == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Link.class.getSimpleName()));
         }
-        if (!Link.isValidConstructionLink(email)) {
-            throw new IllegalValueException(Link.formExceptionMessage(email));
+        if (!Link.isValidConstructionLink(link)) {
+            throw new IllegalValueException(Link.formExceptionMessage(link));
         }
-        final Link modelLink = new Link(email);
+        final Link modelLink = new Link(link);
 
         if (address == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName()));
