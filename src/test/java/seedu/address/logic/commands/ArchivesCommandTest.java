5b5e3da114a83411049d288b40d39dfc15176d6a
Jonathan Cheng <36514601+epicfailname@users.noreply.github.com>
2019-03-25 23:55:07 +0800

    Add stubs for future tests

---
+++ src/test/java/seedu/address/logic/commands/ArchivesCommandTest.java
@@ -0,0 +1,35 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.mocks.TypicalModelManagerStub;
+import seedu.address.model.Model;
+import seedu.address.model.ModelContext;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
+ */
+public class ArchivesCommandTest {
+
+    private Model model = new TypicalModelManagerStub();
+    private Model expectedModel = new TypicalModelManagerStub();
+    private CommandHistory commandHistory = new CommandHistory();
+
+    /*
+    @Before
+    public void setUp() {
+        expectedModel.setContext(ModelContext.CONTEXT_ARCHIVE);
+        expectedModel.setDisplayEntryList(expectedModel.getArchivesEntryBook().getEntryList());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ArchivesCommand(), model, commandHistory,
+            ArchivesCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+    */
+}
