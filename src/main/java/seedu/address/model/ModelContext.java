e944f5241f2bc0a1507164f6362eec219612580d
Qi Ji <qjqqyy@gmail.com>
2019-04-02 15:33:09 +0800

    Feeds context: link it to FeedsParser

--- src/main/java/seedu/address/model/ModelContext.java
+++ src/main/java/seedu/address/model/ModelContext.java
@@ -1,6 +1,7 @@
 package seedu.address.model;
 
 import seedu.address.logic.parser.EntryBookArchivesParser;
+import seedu.address.logic.parser.EntryBookFeedsParser;
 import seedu.address.logic.parser.EntryBookListParser;
 import seedu.address.logic.parser.EntryBookParser;
 import seedu.address.logic.parser.EntryBookSearchParser;
@@ -12,7 +13,7 @@ public enum ModelContext {
     CONTEXT_LIST(new EntryBookListParser(), "Reading List"),
     CONTEXT_ARCHIVES(new EntryBookArchivesParser(), "Archives"),
     CONTEXT_SEARCH(new EntryBookSearchParser(), "Results"),
-    CONTEXT_FEEDS(new EntryBookParser(), "Feeds");
+    CONTEXT_FEEDS(new EntryBookFeedsParser(), "Feeds");
 
     private EntryBookParser parser;
     private String friendlyName;
