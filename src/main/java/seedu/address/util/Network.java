2ef7ca2201742004f96984338ba390d88ef7badc
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2019-04-07 01:35:58 +0800

    Add async versions to Network

--- src/main/java/seedu/address/util/Network.java
+++ src/main/java/seedu/address/util/Network.java
@@ -5,11 +5,26 @@ import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.Dsl;
+import org.asynchttpclient.Response;
 
 /**
  * Manager of Network component
  */
 public abstract class Network {
+
+    private static final int CONNECTION_TIMEOUT_MILLIS = 1000 * 10; // 10 seconds
+    private static final int READ_TIMEOUT_MILLIS = 1000 * 10; // 10 seconds
+    private static final int REQUEST_TIMEOUT_MILLIS = 1000 * 10; // 10 seconds
+
+    private static final AsyncHttpClient asyncHttpClient = Dsl.asyncHttpClient(Dsl.config()
+        .setConnectTimeout(CONNECTION_TIMEOUT_MILLIS)
+        .setReadTimeout(READ_TIMEOUT_MILLIS)
+        .setRequestTimeout(REQUEST_TIMEOUT_MILLIS));;
+
     /**
      * Fetches the resource (i.e. webpage) at url, returning it as an InputStream.
      * @param url
@@ -41,6 +56,21 @@ public abstract class Network {
     }
 
     /**
+     * Fetches the resource (i.e. webpage) at url, returning it as a byte array
+     * @param url
+     * @return The content fetched.
+     * @throws IOException
+     */
+    public static CompletableFuture<byte[]> fetchAsBytesAsync(URL url) {
+        return asyncHttpClient
+            .prepareGet(url.toString())
+            .execute()
+            .toCompletableFuture()
+            .thenApply(Response::getResponseBody)
+            .thenApply(String::getBytes);
+    }
+
+    /**
      * Fetches the article linked at the URL and returns it,
      * but first pre-processing it by converting all links to absolute form.
      */
@@ -64,4 +94,15 @@ public abstract class Network {
                 url,
                 articleContent);
     }
+
+    /**
+     * Fetches the article linked at the URL and returns it,
+     * but first pre-processing it by converting all links to absolute form.
+     */
+    public static CompletableFuture<byte[]> fetchArticleAsync(URL url) {
+        return fetchAsBytesAsync(url)
+            // Convert all links in article to absolute links
+            .thenApply(articleContent -> AbsoluteUrlDocumentConverter.convert(url, articleContent));
+    }
+
 }
