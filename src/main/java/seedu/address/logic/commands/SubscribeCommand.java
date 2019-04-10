b901bf36db3147bd6b884f1d7a9f253367cbc234
Qi Ji <qjqqyy@gmail.com>
2019-04-11 04:45:51 +0800

    feeds/{sub,refresh}: tag entries
    when subscribing to or refreshing a feed we now tag the related entries
    with whatever the feed is also tagged with

--- src/main/java/seedu/address/logic/commands/SubscribeCommand.java
+++ src/main/java/seedu/address/logic/commands/SubscribeCommand.java
@@ -7,7 +7,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TITLE;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Collections;
 
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
@@ -75,7 +74,7 @@ public class SubscribeCommand extends Command {
         model.addFeedsEntry(updatedToSubscribe);
 
         // initial import into reading list
-        EntryBook feedEntries = FeedUtil.serializeToEntryBook(feed, feedUrl.toString(), Collections.emptySet());
+        EntryBook feedEntries = FeedUtil.serializeToEntryBook(feed, feedUrl.toString(), updatedToSubscribe.getTags());
         feedEntries.getEntryList().stream()
                 .filter(entry -> !model.hasEntry(entry))
                 .forEach(entry -> model.addListEntry(entry, Network.fetchArticleOptional(entry.getLink().value)));
