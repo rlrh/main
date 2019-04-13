8c82b0d16f1066c522de7df8fb8f5d26b7f9467a
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2019-04-13 22:34:06 +0800

    Implement CSS url conversion

--- src/main/java/seedu/address/util/AbsoluteUrlDocumentConverter.java
+++ src/main/java/seedu/address/util/AbsoluteUrlDocumentConverter.java
@@ -1,11 +1,23 @@
 package seedu.address.util;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.w3c.css.sac.CSSException;
+import org.w3c.css.sac.InputSource;
+import org.w3c.dom.css.CSSStyleDeclaration;
+import org.w3c.dom.css.CSSValue;
+
+import com.steadystate.css.parser.CSSOMParser;
+import com.steadystate.css.parser.SACParserCSS3;
+import com.steadystate.css.util.ThrowCssExceptionErrorHandler;
 
 /**
  * Converts all the urls in a document to its absolute equivalent,
@@ -40,9 +52,56 @@ public abstract class AbsoluteUrlDocumentConverter {
                 e.attr("src", convertUrl(baseUrl, link));
             }
 
+            if (e.hasAttr("style")) {
+                String cssAttr = e.attr("style");
+                e.attr("style", convertCssUrls(baseUrl, cssAttr));
+            }
+
         }
 
         return document;
+    }
+
+    /**
+     * Converts urls in a CSS style declaration to absolute form.
+     */
+    private static String convertCssUrls(URL baseUrl, String cssAttribute) {
+        CSSOMParser cssParser = new CSSOMParser(new SACParserCSS3());
+        cssParser.setErrorHandler(new ThrowCssExceptionErrorHandler());
+        Pattern urlPattern = Pattern.compile("url\\((.*)\\)");
+        try {
+            // Parse style
+            CSSStyleDeclaration cssStyleDecl = cssParser.parseStyleDeclaration(
+                new InputSource(new StringReader(cssAttribute)));
+
+            // Loop through each attribute in decl
+            for (int i = 0; i < cssStyleDecl.getLength(); i++) {
+
+                // Check if it matches url(...) pattern
+                String property = cssStyleDecl.item(i);
+                CSSValue propertyValue = cssStyleDecl.getPropertyCSSValue(property);
+                String propertyPriority = cssStyleDecl.getPropertyPriority(property);
+                Matcher urlPropMatch = urlPattern.matcher(propertyValue.toString());
+                if (!urlPropMatch.matches()) {
+                    continue;
+                }
+
+                // If it does, convert link
+                String url = urlPropMatch.toMatchResult().group(1);
+                String convertedUrl = convertUrl(baseUrl, url);
+                String newPropertyValue = "url(" + convertedUrl + ")";
+
+                // And set it back in the style decl
+                cssStyleDecl.setProperty(property, newPropertyValue, propertyPriority);
+            }
+
+            return cssStyleDecl.getCssText();
+        } catch (IOException | CSSException ioe) {
+            // IOException will never happen since we use InputSource StringReader combo
+            // If CSS parsing had error, preserve style completely.
+            return cssAttribute;
+        }
+
     }
 
     /**
