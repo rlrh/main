07c1aaa3aaa0df7ca51e2e63a92cc0cf22cff5aa
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2019-04-14 18:37:33 +0800

    Add missing parseCommand for refreshall in feeds context

--- src/test/java/seedu/address/logic/parser/EntryBookFeedsParserTest.java
+++ src/test/java/seedu/address/logic/parser/EntryBookFeedsParserTest.java
@@ -8,6 +8,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.address.logic.commands.RefreshAllFeedsCommand;
 import seedu.address.logic.commands.RefreshFeedCommand;
 import seedu.address.logic.commands.UnsubscribeCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -24,6 +25,13 @@ public class EntryBookFeedsParserTest {
         RefreshFeedCommand command = (RefreshFeedCommand) parser.parseCommand(
                 RefreshFeedCommand.COMMAND_WORD + " " + INDEX_FIRST_ENTRY.getOneBased());
         assertEquals(new RefreshFeedCommand(INDEX_FIRST_ENTRY), command);
+    }
+
+    @Test
+    public void parseCommand_refreshall() throws Exception {
+        RefreshAllFeedsCommand command = (RefreshAllFeedsCommand) parser.parseCommand(
+            RefreshAllFeedsCommand.COMMAND_WORD);
+        assertEquals(new RefreshAllFeedsCommand(), command);
     }
 
     @Test
