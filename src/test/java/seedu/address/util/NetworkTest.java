5defdcb332bdab8079876940cade7822c607e46e
rlrh <rlrh1996@gmail.com>
2019-04-14 22:45:49 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/util/NetworkTest.java
+++ src/test/java/seedu/address/util/NetworkTest.java
@@ -1,6 +1,10 @@
 package seedu.address.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static seedu.address.testutil.TypicalEntries.FILE_TEST_CONTENTS;
 import static seedu.address.testutil.TypicalEntries.VALID_FILE_LINK;
 import static seedu.address.testutil.TypicalEntries.VALID_HTTPS_LINK;
